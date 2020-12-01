0a8ad85d559d7d51cd999b1a2f5dc13ec340ff0a
Andreas Murtonen <Andreas@Andreas-MBP.home>
2020-12-01 17:52:31 +0200

    project initialized

---
+++ config/database.php
@@ -0,0 +1,7 @@
+<?php
+$DB_HOST = 'mysql:host=localhost';
+$DB_BASE = 'db_camagru';
+$DB_DSN = $DB_HOST . ';dbname=' . $DB_BASE;
+$DB_USER = 'root';
+$DB_PASSWORD = 'password';
+?>
---
+++ config/setup.php
@@ -0,0 +1,86 @@
+<?php
+echo "init db installation muddafucka";
+require_once('database.php');
+// create DATABASE
+try {
+    $db = new PDO($DB_HOST, $DB_USER, $DB_PASSWORD);
+    // set the PDO error mode to exception
+    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    $sql = "CREATE DATABASE IF NOT EXISTS db_camagru";
+    $db->exec($sql);
+} catch (PDOException $e) {
+    echo "ERROR CREATING DATABASE: " . $e->getMessage() . "Aborting process<br>";
+}
+echo "<br>";
+try {
+    //create Table 'users'
+    $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE IF NOT EXISTS `users` (
+        `user_id` int(11) NOT NULL AUTO_INCREMENT,
+        `username` varchar(50) NOT NULL,
+        `email` varchar(250) NOT NULL,
+        `verified` tinyint(1) NOT NULL DEFAULT '0',
+        `password` varchar(255) NOT NULL,
+        `recieveCommentEmail` varchar(255) NOT NULL DEFAULT '0',
+        `creationDate` DATETIME NOT NULL,
+        PRIMARY KEY (`user_id`)
+    )";
+    $dbh->exec($sql);
+} catch (PDOException $e) {
+    echo "ERROR CREATING USERS TABLE: " . $e->getMessage() . "Aborting process<br>";
+}
+echo "<br>";
+try {
+    //create Table 'posts'
+    $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE IF NOT EXISTS `posts` (
+        `post_id` int(11) NOT NULL AUTO_INCREMENT,
+        `photoName` varchar(500) NOT NULL,
+        `creationDate` DATETIME NOT NULL,
+        `user_id` int(11),
+        FOREIGN KEY (`user_id`) REFERENCES users(`user_id`),
+        PRIMARY KEY (`post_id`)
+    )";
+    $dbh->exec($sql);
+} catch (PDOException $e) {
+    echo "ERROR CREATING posts TABLE: " . $e->getMessage() . "Aborting process<br>";
+}
+echo "<br>";
+try {
+    //create Table 'comments'
+    $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE IF NOT EXISTS `comments` (
+        `comments_id` int(11) NOT NULL AUTO_INCREMENT,
+        `comment` varchar(3000) NOT NULL,
+        `creationDate` DATETIME NOT NULL,
+        `user_id` int(11),
+        FOREIGN KEY (`user_id`) REFERENCES users(`user_id`),
+        `post_id` int(11),
+        FOREIGN KEY (`post_id`) REFERENCES posts(`post_id`),
+        PRIMARY KEY (`comments_id`)
+    )";
+    $dbh->exec($sql);
+} catch (PDOException $e) {
+    echo "ERROR CREATING comments TABLE: " . $e->getMessage() . "Aborting process<br>";
+}
+echo "<br>";
+try {
+    //create Table 'likes'
+    $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE IF NOT EXISTS `likes` (
+        `like_id` int(11) NOT NULL AUTO_INCREMENT,
+        `user_id` int(11),
+        FOREIGN KEY (`user_id`) REFERENCES users(`user_id`),
+        `post_id` int(11),
+        FOREIGN KEY (`post_id`) REFERENCES posts(`post_id`),
+        PRIMARY KEY (`like_id`)
+    )";
+    $dbh->exec($sql);
+} catch (PDOException $e) {
+    echo "ERROR CREATING comments TABLE: " . $e->getMessage() . "Aborting process<br>";
+}
+echo "success muddafucka";
